cf1f0fcfcf335e7dc6c892ac52d0a16714f63c57
Aya Alsakkaf <aya.alsakkaf@ccse.ku.edu.kw>
2024-02-04 21:58:49 +0300

    User permissions message

--- src/pages/UserRecipes.js
+++ src/pages/UserRecipes.js
@@ -2,9 +2,10 @@ import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import { getUserRecipes } from "../api/recipes";
 import RecipeCard from "../components/RecipeCard";
+import Alert from "../components/Alert";
 
 const UserRecipes = () => {
-  const { data: userRecipes } = useQuery({
+  const { data: userRecipes, isError } = useQuery({
     queryKey: ["userRecipes"],
     queryFn: getUserRecipes,
   });
@@ -17,9 +18,22 @@ const UserRecipes = () => {
     />
   ));
   return (
-    <div className="grid place-items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-20 m-10">
-      {displayRecipes}
-    </div>
+    <>
+      <div className="">
+        <h1 className="text-3xl  text-center mt-10 mainFont">Your Recipes</h1>
+      </div>
+      {isError && (
+        <div className="flex justify-center">
+          <Alert
+            text="You need an account to view your recipes"
+            bg="bg-red-700 text-white text-center w-[50%]"
+          />
+        </div>
+      )}
+      <div className="grid place-items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-20 m-10">
+        {displayRecipes}
+      </div>
+    </>
   );
 };
 
